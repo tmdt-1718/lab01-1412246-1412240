c538b49a3e054830261d2ce543762deaff788739
1412240 <rubify@Rubifys-Mac-mini.local>
2017-09-26 13:35:59 +0700

    fix devise and authenicate

--- lab01/app/controllers/blogs_controller.rb
+++ lab01/app/controllers/blogs_controller.rb
@@ -1,13 +1,37 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new,:edit,:create,:destroy]
+  before_action :check_user, only:[:edit]
   def index
   end
 
   def new
+    @blog = Blog.new
 
   end
+
+  def edit
+  end
+
   def create
+    blog = Blog.new(blog_params)
+    blog.user_id = current_user.id
+    blog.save
   end
   private
+  def set_blog
+  @blog = Blog.find(params[:id])
+  end
+
+  def check_user
+    if user_signed_in?
+      if @blog.user_id.to_i != current_user.id
+          redirect_to root_path
+      end
+    else
+        redirect_to root_path
+    end
+  end
   def blog_params
     params.require(:blog).permit(:title,:content,:status,:avatar)
   end
--- lab01/config/routes.rb
+++ lab01/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   root 'home#index'
-  get '/newblog' => "blogs#new"
+  get '/newblog' => "blogs#new", as: "new_blog"
   post '/newblog' => "blog#create" , as: "blogs"
-  devise_for :users,controllers: { sessions: 'users/sessions' }
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
