a72c8cb1b323a13a1b329b3c500f804c71380023
Bui Dinh Khanh 1412240 <bdkhanh1996@gmail.com>
2017-10-07 21:28:27 +0700

    merge

--- lab01/app/models/album.rb
+++ lab01/app/models/album.rb
@@ -2,5 +2,5 @@ class Album < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/Missing.jpg"
  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
  belongs_to :user
- has_many :images
+ has_many :images , dependent: :destroy
 end
--- lab01/app/models/blog.rb
+++ lab01/app/models/blog.rb
@@ -2,7 +2,7 @@ class Blog < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/Missing.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   def self.search (name)
       if name
